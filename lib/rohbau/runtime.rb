01bcb111e39477578f1ce0bbc1974a04669bed3c
Jakob Holderbaum <jh@lambda.neopoly.de>
2013-10-15 09:41:45 +0200

    Add manager runtime

---
+++ lib/rohbau/runtime.rb
@@ -0,0 +1,57 @@
+require 'pathname'
+
+module Rohbau
+  class Runtime
+    class << self
+      attr_reader :instance
+
+      def start
+        @instance = new
+      end
+
+      def running?
+        !!@instance
+      end
+
+      def terminate
+        @instance.terminate
+        @instance = nil
+      end
+    end
+
+    def self.register(name, plugin_class)
+      attr_reader name
+      plugins[name] = plugin_class
+    end
+
+    def self.plugins
+      @@plugins ||= {}
+    end
+
+    def initialize
+      initialize_plugins
+      on_boot
+    end
+
+    def on_boot
+      # noop
+    end
+
+    def terminate
+      # noop
+    end
+
+    def root
+      Pathname.new(Dir.pwd)
+    end
+
+    protected
+
+    def initialize_plugins
+      self.class.plugins.each do |name, plugin_class|
+        instance_variable_set :"@#{name}", plugin_class.new(self)
+      end
+    end
+
+  end
+end
