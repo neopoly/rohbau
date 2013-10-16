8e24cc7f281e8c8aeb0edf9c4015bea9ce2b4137
Jakob Holderbaum <jh@neopoly.de>
2013-10-16 20:51:16 +0200

    Add synchronous EventTube

---
+++ lib/rohbau/event_tube.rb
@@ -0,0 +1,38 @@
+module Rohbau
+  class EventTube
+    def self.publish(name, event)
+      subscription_handler.handle(name, event)
+    end
+
+    def self.subscribe(name, &handler)
+      subscription_handler.add(name, &handler)
+    end
+
+    def self.subscription_handler
+      @subscriptions ||= SubscriptionHandler.new
+    end
+
+    class SubscriptionHandler
+
+      def add(name, &handler)
+        subscriptions[name] << handler
+        true
+      end
+
+      def handle(name, event)
+        subscriptions[name].each do |handler|
+          handler.call(event)
+        end
+        true
+      end
+
+      private
+      def subscriptions
+        @subscriptions ||= Hash.new do |h,k|
+          h[k] = []
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/event_tube_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+require 'rohbau/event_tube'
+
+describe Rohbau::EventTube do
+  let(:tube)        { Class.new(Rohbau::EventTube)}
+  let(:event_class) { Struct.new(:arg1) }
+  let(:event)       { event_class.new(22) }
+
+  describe 'without subscribers' do
+    it 'can publish' do
+      tube.publish :my_event, event
+    end
+  end
+
+  describe 'with subscriber' do
+    before do
+      @calls = []
+      tube.subscribe :my_event do |event|
+        @calls << event
+      end
+    end
+
+    it 'can publish' do
+      tube.publish :my_event, event
+      assert_equal [event], @calls
+    end
+  end
+
+  describe 'with multiple subscribers' do
+    before do
+      @calls = []
+
+      tube.subscribe :my_event do |event|
+        @calls << event
+      end
+
+      tube.subscribe :my_event do |event|
+        @calls << event
+      end
+    end
+
+    it 'processes them all on publish' do
+      tube.publish :my_event, event
+      assert_equal [event,event], @calls
+    end
+  end
+
+end
