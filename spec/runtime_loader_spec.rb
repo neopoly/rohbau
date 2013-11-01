8ca21438801aa0efbf2bf46428f84b46207a7fc0
Jakob Holderbaum <jh@neopoly.de>
2013-11-01 10:58:30 +0100

    Refactor RuntimeLoader spec

--- spec/runtime_loader_spec.rb
+++ spec/runtime_loader_spec.rb
@@ -11,15 +11,15 @@ describe Rohbau::RuntimeLoader do
     Class.new
   end
 
-  it 'starts up a given class' do
+  before do
     my_runtime_loader.new(example_class)
+  end
 
+  it 'starts up a given class' do
     assert_kind_of example_class, my_runtime_loader.instance
   end
 
   it 'fails if it was already instanciated' do
-    my_runtime_loader.new(example_class)
-
     raised = assert_raises RuntimeError do
       my_runtime_loader.new(example_class)
     end
