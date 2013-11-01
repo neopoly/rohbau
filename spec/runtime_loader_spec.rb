d4116b2269d62ba2963eaa2a062c8ba8347f51e8
Jakob Holderbaum <jh@neopoly.de>
2013-11-01 11:13:58 +0100

    Refactor RuntimeLoader spec to use example Class

--- spec/runtime_loader_spec.rb
+++ spec/runtime_loader_spec.rb
@@ -2,26 +2,23 @@ require 'spec_helper'
 require 'rohbau/runtime_loader'
 
 describe Rohbau::RuntimeLoader do
+  ExampleClass = Class.new
 
   let(:my_runtime_loader) do
     Class.new(Rohbau::RuntimeLoader)
   end
 
-  let(:example_class) do
-    Class.new
-  end
-
   before do
-    my_runtime_loader.new(example_class)
+    my_runtime_loader.new(ExampleClass)
   end
 
   it 'starts up a given class' do
-    assert_kind_of example_class, my_runtime_loader.instance
+    assert_kind_of ExampleClass, my_runtime_loader.instance
   end
 
   it 'fails if it was already instanciated' do
     raised = assert_raises RuntimeError do
-      my_runtime_loader.new(example_class)
+      my_runtime_loader.new(ExampleClass)
     end
 
     assert_match(/already/, raised.message)
