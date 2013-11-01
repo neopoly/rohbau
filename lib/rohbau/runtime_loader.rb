31a78fa07b71549994f0147bf12499256b75de23
Jakob Holderbaum <jh@neopoly.de>
2013-11-01 10:57:41 +0100

    Refactor RuntimeLoader

--- lib/rohbau/runtime_loader.rb
+++ lib/rohbau/runtime_loader.rb
@@ -6,12 +6,32 @@ module Rohbau
     end
 
     def initialize(runtime)
-      if self.class.instance_variable_defined? :@instance
-        instance = self.class.instance_variable_get :@instance
-        msg = "Runtime already initialized: #{instance.inspect}"
+      ensure_singleton_is_unassigned!
+      assign_to_singleton(runtime.new)
+    end
+
+    private
+    def ensure_singleton_is_unassigned!
+      if singleton_assigned?
+        msg = "Runtime already initialized: #{singleton.inspect}"
         raise RuntimeError, msg
       end
-      self.class.instance_variable_set :@instance, runtime.new
+    end
+
+    def singleton_assigned?
+      self.class.instance_variable_defined? singleton_variable
+    end
+
+    def singleton
+      self.class.instance_variable_get singleton_variable
+    end
+
+    def assign_to_singleton(instance)
+      self.class.instance_variable_set singleton_variable, instance
+    end
+
+    def singleton_variable
+      :@instance
     end
   end
 end
