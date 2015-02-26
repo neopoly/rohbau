dd388c662b662b1655ebdc9a3cf435ddd4307b43
Jan Owiesniak <jo@neopoly.de>
2015-02-26 16:09:53 +0100

    Fix spaces

--- etc/build_readme.rb
+++ etc/build_readme.rb
@@ -4,7 +4,7 @@ examples = template.scan(/include_example (.*)/)[0]
 
 examples.each do |example|
   file = "examples/#{example}.rb"
-    if File.exists?(file)
+  if File.exists?(file)
     replacement = File.read(file)
     template.gsub!(/include_example #{example}/, replacement)
   end
