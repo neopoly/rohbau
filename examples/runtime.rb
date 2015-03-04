c79a2f17b637ec702185b51e54d337781f7ba084
Jan Owiesniak <jowiesniak@gmail.com>
2015-03-04 12:15:31 +0100

    Extract UserService from runtime example

--- examples/runtime.rb
+++ examples/runtime.rb
@@ -1,18 +1,6 @@
-require 'rohbau/runtime'
-require 'rohbau/runtime_loader'
+require 'user_service'
 
 module MyApplication
-  class RuntimeLoader < Rohbau::RuntimeLoader
-    def initialize
-      super(Runtime)
-    end
-  end
-
-  class Runtime < Rohbau::Runtime
-  end
-end
-
-module UserService
   class RuntimeLoader < Rohbau::RuntimeLoader
     def initialize
       super(Runtime)
