c15229d4478d619bfb789216bd98eddfc4c85f12
Peter Suschlik <ps@neopoly.de>
2015-03-05 10:52:29 +0100

    Document rohbau.rb

--- lib/rohbau.rb
+++ lib/rohbau.rb
@@ -1,5 +1,23 @@
 require "rohbau/version"
 
+# Rohbau provides a set of patterns used in Domain Driven Design.
+#
+# Require individual parts of Rohbau to use its functionality
+#
+# @example
+#     require 'rohbau/runtime'
+#     require 'rohbau/runtime_loader'
+#     module MyApplication
+#       class RuntimeLoader < Rohbau::RuntimeLoader
+#         def initialize
+#           super(Runtime)
+#         end
+#       end
+#
+#       class Runtime < Rohbau::Runtime
+#       end
+#     end
+#
+# @see README for more examples
 module Rohbau
-  # Your code goes here...
 end
