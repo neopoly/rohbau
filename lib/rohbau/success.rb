e90c21ccb4a4adc2f97d893585408c06492f9211
Jan Owiesniak <jo@neopoly.de>
2016-06-17 13:42:38 +0200

    Implement success / failure bound

---
+++ lib/rohbau/success.rb
@@ -0,0 +1,14 @@
+require 'bound'
+
+module Rohbau
+  class Success < Bound
+    private
+
+    def self.new_bound_class
+      Class.new(StaticBoundClass) do |cls|
+        cls.define_singleton_method(:success?) { true }
+        cls.define_singleton_method(:failure?) { false }
+      end
+    end
+  end
+end
